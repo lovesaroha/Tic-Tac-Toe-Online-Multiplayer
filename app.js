499983e0436fe0255cd12f10a53d09b1aa7563f3
lovesaroha <lovesaroha1994@gmail.com>
2021-09-30 23:29:34 +0530

    first commit

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var rooms = {};
 const combinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
 
-// Port 3000.
-const PORT = 3000;
+// Port 3002.
+const PORT = 3002;
 console.log(`Server running at port ${PORT}`);
 server.listen(PORT);
 
